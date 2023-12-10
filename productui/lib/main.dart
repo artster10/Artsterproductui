88373620371e0562a3cb4465e76310b1a5f0e43b
faseem <mohdfaseem7@gmail.com>
2023-12-10 12:21:23 +0530

    .

--- productui/lib/main.dart
+++ productui/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 33, 85, 26)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 228, 252, 225)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
