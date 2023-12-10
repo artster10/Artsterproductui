7932a2e78d0d2fae728ce78abe8359350de79897
faseem <mohdfaseem7@gmail.com>
2023-12-10 17:34:19 +0530

    completed starting screen

--- productui/lib/main.dart
+++ productui/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:productui/onboarding/onboarding_screen.dart';
+import 'config/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +13,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Artster',
+      title: 'Artster',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: const Scaffold(),
+      theme: kAppThemeData,
+      home: const OnboardingScreen(),
     );
   }
+}
+
+final ThemeData kAppThemeData = _buildAppTheme();
+
+ThemeData _buildAppTheme() {
+  final base = ThemeData.light();
+  final baseTextTheme = GoogleFonts.poppinsTextTheme(base.textTheme);
+  return base.copyWith(
+    scaffoldBackgroundColor: kWhite,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+    ),
+    textTheme: baseTextTheme.copyWith(
+      // Title
+      displayLarge: baseTextTheme.displayLarge!.copyWith(
+        height: 58.5 / 39,
+        fontWeight: FontWeight.w700,
+        fontSize: 39.0,
+        color: kBlack,
+      ),
+      // H1
+      displayMedium: baseTextTheme.displayMedium!.copyWith(
+        height: 46.88 / 31.25,
+        fontWeight: FontWeight.w700,
+        fontSize: 31.25,
+        color: kBlack,
+      ),
+      // H2
+      displaySmall: baseTextTheme.displaySmall!.copyWith(
+        height: 37.5 / 25.0,
+        fontWeight: FontWeight.w400,
+        fontSize: 25.0,
+        color: kBlack,
+      ),
+      // H3, Button Large
+      headlineSmall: baseTextTheme.headlineSmall!.copyWith(
+        height: 30.0 / 20.0,
+        fontWeight: FontWeight.w400,
+        fontSize: 20.0,
+        color: kBlack,
+      ),
+      //Body Regular (Body Copy), Button Regular
+      bodyLarge: baseTextTheme.bodyLarge!.copyWith(
+        color: kBlack,
+        fontWeight: FontWeight.w400,
+      ),
+      // Body Small
+      bodySmall: baseTextTheme.bodySmall!.copyWith(
+        height: 21.0 / 14.0,
+        fontSize: 14.0,
+        fontWeight: FontWeight.w400,
+        color: kBlack,
+      ),
+      // Caption
+      labelSmall: baseTextTheme.labelSmall!.copyWith(
+        height: 19.2 / 12.8,
+        fontSize: 12.8,
+        color: kCaption,
+        fontWeight: FontWeight.w400,
+      ),
+    ),
+  );
 }
---
+++ productui/lib/onboarding/widgets/background.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:productui/config/colors.dart';
+import 'dart:math' as math;
+
+class Background extends StatelessWidget {
+  const Background({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+          top: size.height * -0.3,
+          left: size.height * -0.65,
+          child: Transform.rotate(
+            angle: math.pi / 4,
+            child: Container(
+              height: size.height,
+              width: size.height,
+              padding: EdgeInsets.all(40.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(155.0),
+                border: Border.all(width: 1.0, color: k3AccentLines),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(152.0),
+                    color: k2MainThemeColor),
+              ),
+            ),
+          ),
+        ),
+        child,
+      ],
+    ));
+  }
+}
