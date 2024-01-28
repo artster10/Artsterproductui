e37cd4e21ed47711d7a67e38d8279ec7f15797b5
faseem <mohdfaseem7@gmail.com>
2024-01-28 13:18:56 +0530

    video

--- productui/lib/screens/view_product/product.dart
+++ productui/lib/screens/view_product/product.dart
@@ -20,18 +20,16 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
-
-  _ProductPageState(){
+  _ProductPageState() {
     view_notification();
   }
 
   List<String> id_ = <String>[];
-  List<String> pname_= <String>[];
-  List<String> price_= <String>[];
-  List<String> video_= <String>[];
-  List<String> images_= <String>[];
-  List<String> pinfo_= <String>[];
-
+  List<String> pname_ = <String>[];
+  List<String> price_ = <String>[];
+  List<String> video_ = <String>[];
+  List<String> images_ = <String>[];
+  List<String> pinfo_ = <String>[];
 
   Future<void> view_notification() async {
     List<String> id = <String>[];
@@ -41,19 +39,12 @@ class _ProductPageState extends State<ProductPage> {
     List<String> images = <String>[];
     List<String> video = <String>[];
 
-
-
-
-
     try {
       SharedPreferences sh = await SharedPreferences.getInstance();
       String urls = sh.getString('url').toString();
       String url = '$urls/and_view_product/';
 
-      var data = await http.post(Uri.parse(url), body: {
-
-
-      });
+      var data = await http.post(Uri.parse(url), body: {});
       var jsondata = json.decode(data.body);
       String statuss = jsondata['status'];
 
@@ -63,13 +54,11 @@ class _ProductPageState extends State<ProductPage> {
 
       for (int i = 0; i < arr.length; i++) {
         id.add(arr[i]['id'].toString());
-        images.add(sh.getString('img_url').toString()+arr[i]['images']);
+        images.add(sh.getString('img_url').toString() + arr[i]['images']);
         pname.add(arr[i]['pname']);
         pinfo.add(arr[i]['pinfo']);
-        video.add(sh.getString("img_url").toString()+arr[i]['video']);
+        video.add(sh.getString("img_url").toString() + arr[i]['video']);
         price.add(arr[i]['price']);
-
-
       }
 
       setState(() {
@@ -79,9 +68,6 @@ class _ProductPageState extends State<ProductPage> {
         pname_ = pname;
         pinfo_ = pinfo;
         price_ = price;
-
-
-
       });
 
       print(statuss);
@@ -94,204 +80,207 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Background(child: Scaffold(
-      backgroundColor: Colors.transparent,
-      // appBar: AppBar(
-      //   automaticallyImplyLeading: false,
-      //   centerTitle: false,
-      //   title: Padding(
-      //     padding: const EdgeInsets.only(left: 8.0),
-      //     child: Text(
-      //       'Artster',
-      //       style: Theme.of(context)
-      //           .textTheme
-      //           .bodyLarge!
-      //           .copyWith(fontWeight: FontWeight.w700),
-      //     ),
-      //   ),
-      //   actions: [
-      //     Padding(
-      //       padding: const EdgeInsets.only(right: 16.0),
-      //       child: IconButton(
-      //         onPressed: () {},
-      //         icon: SvgPicture.asset('assets/icons/search.svg'),
-      //       ),
-      //     ),
-      //   ],
-      // ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 42,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Text(
-                'Feed',
-                style: Theme.of(context)
-                    .textTheme
-                    .displaySmall!
-                    .copyWith(fontWeight: FontWeight.w700),
+    return Background(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        // appBar: AppBar(
+        //   automaticallyImplyLeading: false,
+        //   centerTitle: false,
+        //   title: Padding(
+        //     padding: const EdgeInsets.only(left: 8.0),
+        //     child: Text(
+        //       'Artster',
+        //       style: Theme.of(context)
+        //           .textTheme
+        //           .bodyLarge!
+        //           .copyWith(fontWeight: FontWeight.w700),
+        //     ),
+        //   ),
+        //   actions: [
+        //     Padding(
+        //       padding: const EdgeInsets.only(right: 16.0),
+        //       child: IconButton(
+        //         onPressed: () {},
+        //         icon: SvgPicture.asset('assets/icons/search.svg'),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 42,
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  'Feed',
+                  style: Theme.of(context)
+                      .textTheme
+                      .displaySmall!
+                      .copyWith(fontWeight: FontWeight.w700),
+                ),
+              ),
 
-            // SingleChildScrollView(
-            //   scrollDirection: Axis.horizontal,
-            //   child: Row(
-            //     children: [
-            //       Container(
-            //         height: 56.0,
-            //         width: 56.0,
-            //         alignment: Alignment.center,
-            //         margin: const EdgeInsets.only(left: 24.0),
-            //         decoration: BoxDecoration(
-            //           shape: BoxShape.circle,
-            //           gradient: k3GradientAccent,
-            //           boxShadow: [
-            //             BoxShadow(
-            //               blurRadius: 12.0,
-            //               offset: const Offset(0, 4),
-            //               color: k3Pink.withOpacity(0.52),
-            //             ),
-            //           ],
-            //         ),
-            //         child: SvgPicture.asset('assets/icons/only_plus.svg'),
-            //       ),
-            //       ...List.generate(
-            //         5,
-            //         (index) => Container(
-            //           height: 56.0,
-            //           width: 56.0,
-            //           margin: EdgeInsets.only(
-            //             left: 30.0,
-            //             right: index == 4 ? 30.0 : 0.0,
-            //           ),
-            //           alignment: Alignment.center,
-            //           decoration: BoxDecoration(
-            //             shape: BoxShape.circle,
-            //             border: Border.all(width: 2.0, color: k2AccentStroke),
-            //             image: const DecorationImage(
-            //               image:
-            //                   AssetImage('assets/images/profile_image.jpg'),
-            //               fit: BoxFit.cover,
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: id_.length,
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () async {
+              // SingleChildScrollView(
+              //   scrollDirection: Axis.horizontal,
+              //   child: Row(
+              //     children: [
+              //       Container(
+              //         height: 56.0,
+              //         width: 56.0,
+              //         alignment: Alignment.center,
+              //         margin: const EdgeInsets.only(left: 24.0),
+              //         decoration: BoxDecoration(
+              //           shape: BoxShape.circle,
+              //           gradient: k3GradientAccent,
+              //           boxShadow: [
+              //             BoxShadow(
+              //               blurRadius: 12.0,
+              //               offset: const Offset(0, 4),
+              //               color: k3Pink.withOpacity(0.52),
+              //             ),
+              //           ],
+              //         ),
+              //         child: SvgPicture.asset('assets/icons/only_plus.svg'),
+              //       ),
+              //       ...List.generate(
+              //         5,
+              //         (index) => Container(
+              //           height: 56.0,
+              //           width: 56.0,
+              //           margin: EdgeInsets.only(
+              //             left: 30.0,
+              //             right: index == 4 ? 30.0 : 0.0,
+              //           ),
+              //           alignment: Alignment.center,
+              //           decoration: BoxDecoration(
+              //             shape: BoxShape.circle,
+              //             border: Border.all(width: 2.0, color: k2AccentStroke),
+              //             image: const DecorationImage(
+              //               image:
+              //                   AssetImage('assets/images/profile_image.jpg'),
+              //               fit: BoxFit.cover,
+              //             ),
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: id_.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () async {
+                      SharedPreferences sh =
+                          await SharedPreferences.getInstance();
+                      sh.setString("pid", id_[index]);
+                      // sh.setString("name", pname_[index]);
+                      // sh.setString("description", pinfo_[index]);
+                      // sh.setString("price", price_[index]);
+                      // sh.setString("video", video_[index]);
 
-
-                    SharedPreferences sh = await SharedPreferences.getInstance();
-                    sh.setString("pid", id_[index]);
-                    // sh.setString("name", pname_[index]);
-                    // sh.setString("description", pinfo_[index]);
-                    // sh.setString("price", price_[index]);
-                    // sh.setString("video", video_[index]);
-
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProductInfo(),
-                        ));
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 24.0,
-                      vertical: 32.0,
-                    ),
-                    padding: const EdgeInsets.all(14.0),
-                    height: size.height * 0.40,
-                    width: size.width,
-                    decoration: BoxDecoration(
-                      // color: Colors.red,
-                      borderRadius: BorderRadius.circular(20.0),
-                      image: DecorationImage(
-                        image: NetworkImage(images_[index]),
-                        fit: BoxFit.cover,
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductInfo(),
+                          ));
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 24.0,
+                        vertical: 32.0,
+                      ),
+                      padding: const EdgeInsets.all(14.0),
+                      height: size.height * 0.40,
+                      width: size.width,
+                      decoration: BoxDecoration(
+                        // color: Colors.red,
+                        borderRadius: BorderRadius.circular(20.0),
+                        image: DecorationImage(
+                          image: NetworkImage(images_[index]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Row(
+                                children: [
+                                  const CircleAvatar(
+                                    backgroundImage: AssetImage(
+                                        'assets/images/profile_image.jpg'),
+                                    maxRadius: 16.0,
+                                  ),
+                                  const SizedBox(width: 8.0),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Dennis Reynold',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelSmall!
+                                            .copyWith(color: kWhite),
+                                      ),
+                                      Text(
+                                        '2 hrs ago',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelLarge!
+                                            .copyWith(
+                                                color: const Color(0xFFD8D8D8)),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon:
+                                    const Icon(Icons.more_vert, color: kWhite),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          // Row(
+                          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          //   children: [
+                          //     _buildPostStat(
+                          //       context: context,
+                          //       iconPath: 'assets/icons/favorite_border.svg',
+                          //       value: '5.2K',
+                          //     ),
+                          //     _buildPostStat(
+                          //       context: context,
+                          //       iconPath: 'assets/icons/favorite_border.svg',
+                          //       value: '1.1K',
+                          //     ),
+                          //     _buildPostStat(
+                          //       context: context,
+                          //       iconPath: 'assets/icons/favorite_border.svg',
+                          //       value: '5.2K',
+                          //     ),
+                          //   ],
+                          // )
+                        ],
                       ),
                     ),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  backgroundImage: AssetImage(
-                                      'assets/images/profile_image.jpg'),
-                                  maxRadius: 16.0,
-                                ),
-                                const SizedBox(width: 8.0),
-                                Column(
-                                  crossAxisAlignment:
-                                  CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Dennis Reynold',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelSmall!
-                                          .copyWith(color: kWhite),
-                                    ),
-                                    Text(
-                                      '2 hrs ago',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelLarge!
-                                          .copyWith(
-                                          color: const Color(0xFFD8D8D8)),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            const Spacer(),
-                            IconButton(
-                              onPressed: () {},
-                              icon:
-                              const Icon(Icons.more_vert, color: kWhite),
-                            ),
-                          ],
-                        ),
-                        const Spacer(),
-                        // Row(
-                        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        //   children: [
-                        //     _buildPostStat(
-                        //       context: context,
-                        //       iconPath: 'assets/icons/favorite_border.svg',
-                        //       value: '5.2K',
-                        //     ),
-                        //     _buildPostStat(
-                        //       context: context,
-                        //       iconPath: 'assets/icons/favorite_border.svg',
-                        //       value: '1.1K',
-                        //     ),
-                        //     _buildPostStat(
-                        //       context: context,
-                        //       iconPath: 'assets/icons/favorite_border.svg',
-                        //       value: '5.2K',
-                        //     ),
-                        //   ],
-                        // )
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
-    ),);
+    );
   }
 }
